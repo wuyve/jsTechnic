c3f99d0c28c969788f1308cc3af979c91a30fd51
wuyve <y80608991@126.com>
2019-11-19 20:45:31 +0800

    更新完成 ：数组相关技巧

---
+++ index.js
@@ -0,0 +1,19 @@
+var people = [
+    { name: 'Alice', age: 21 },
+    { name: 'Max', age: 20 },
+    { name: 'Jane', age: 20 }
+  ];
+  
+function groupBy(objectArray, property) {
+return objectArray.reduce(function (acc, obj) {
+    var key = obj[property];
+    if (!acc[key]) {
+    acc[key] = [];
+    }
+    acc[key].push(obj);
+    return acc;
+}, {});
+}
+  
+var groupedPeople = groupBy(people, 'age');
+console.log(groupedPeople);
